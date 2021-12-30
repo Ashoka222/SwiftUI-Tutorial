54b6f4cd010f2208f71e84722d02bc30ba5ec608
Ashok Dattatray Kumbhar <ashokkumbhar222@gmail.com>
2021-12-30 17:03:18 +0530

    created new project
    created new project for SwiftUI Tutorial

---
+++ SwiftUI-Tutorial/SwiftUI_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialApp.swift
+//  SwiftUI-Tutorial
+//
+//  Created by K Ashok on 30/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
