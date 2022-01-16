70cd948ed3f28a47314907b6dd750593a21cb812
Ashok Kumbhar <ashok.kumbhar@mindbowser.com>
2022-01-16 17:18:18 +0530

    Game implementation completed

--- SwiftUI-Tutorial/ContentView.swift
+++ SwiftUI-Tutorial/ContentView.swift
@@ -8,9 +8,81 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var playerCard = "card11"
+    @State var cpuCard = "card12"
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    @State var presentAlert = false
+    
     var body: some View {
-        Text("Hello, world! from Swift UI")
-            .padding()
+        
+        ZStack {
+            Image("background").ignoresSafeArea()
+            VStack {
+                Spacer()
+                Image("logo")
+                    .aspectRatio(contentMode: .fit)
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                Button {
+                    // Genarate random number between 2 to 14
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    // Update card
+                    playerCard = "card\(playerRand)"
+                    cpuCard = "card\(cpuRand)"
+                    
+                    // Update score
+                    playerScore += playerRand>cpuRand ? 1 : 0
+                    cpuScore += playerRand<cpuRand ? 1 : 0
+                    if playerRand == cpuRand {
+                        presentAlert = true                        
+                    }
+                } label: {
+                    Image("dealbutton")
+                }.alert(isPresented: $presentAlert) {
+                    Alert(
+                        title: Text("Yoo!!!"),
+                        message: Text("You both has same card.")
+                    )
+                }
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                    }
+                    .foregroundColor(.white)
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(cpuScore.description)
+                            .font(.largeTitle)
+                    }
+                    .foregroundColor(.white)
+                    Spacer()
+                }
+                Spacer()
+            }
+        }
+//        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        
+        
     }
 }
 
